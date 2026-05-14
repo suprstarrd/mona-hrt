904491578eccdaee54f7b063db2d01b4037a81e0
Délia Cheminot <delia.cheminot@gmail.com>
2026-05-14 18:32:59 +0200

    refactor: remove close button from app bar in ModelForm widget

--- lib/ui/widgets/forms/model_form.dart
+++ lib/ui/widgets/forms/model_form.dart
@@ -29,10 +29,6 @@ class ModelForm extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
-        leading: IconButton(
-          icon: Icon(Icons.close),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
       ),
       body: SafeArea(
         child: DismissKeyboardSingleChildScrollView(
